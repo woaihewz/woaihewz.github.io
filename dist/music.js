a897d9ca10987e0901c2223ed053addc900bb9f6
woaihewz <woaihewz.github.io>
2023-03-30 17:01:44 +0800

    Site updated: 2023-03-30 17:01:44

--- dist/music.js
+++ dist/music.js
@@ -40,7 +40,25 @@ const ap = new APlayer({
       cover: '/images/susan1.jpg',
     },
     {
-      name: 'Friend',
+      name: '庐州月',
+      artist: '许嵩',
+      url: 'http://music.163.com/song/media/outer/url?id=167827.mp3',
+      cover: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRHdVkhTNqyoPCffek9MIchEMIUgAOf-x1jLc9Zu5IXNFlXU9CC',
+    },
+    {
+      name: '惊鸿一面',
+      artist: '许嵩',
+      url: 'http://music.163.com/song/media/outer/url?id=28854182.mp3',
+      cover: 'https://upload.wikimedia.org/wikipedia/zh/thumb/b/b4/Album_Cover_%E4%B8%8D%E5%A6%82%E5%90%83%E8%8C%B6%E5%8E%BB.jpg/220px-Album_Cover_%E4%B8%8D%E5%A6%82%E5%90%83%E8%8C%B6%E5%8E%BB.jpg',
+    },
+    {
+      name: '光るなら',
+      artist: 'Goose house',
+      url: 'http://music.163.com/song/media/outer/url?id=29732992.mp3',
+      cover: 'http://n.sinaimg.cn/front/217/w960h857/20190124/c0S1-hsccyrs3616033.jpg',
+    },
+    {
+      name: 'jhyi',
       artist: 'Kozoro',
       url: 'http://music.163.com/song/media/outer/url?id=33004707.mp3',
       cover: 'http://p2.music.126.net/FR7zKYB8ujNRoWQuGCHbGg==/109951163288692527.jpg?param=130y130',
